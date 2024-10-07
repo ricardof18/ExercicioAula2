d273ba906686c33026808a704f99e55877f0ed3e
ricardof18 <44167524+ricardof18@users.noreply.github.com>
2024-10-07 09:25:38 +0100

    Update java ex

--- app/src/main/java/exercicio/App.java
+++ app/src/main/java/exercicio/App.java
@@ -11,5 +11,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World Three");
         System.out.println("Ola");
+        System.out.println("Exercicio 2");
     }
 }
